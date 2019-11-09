b8ad7776671c5c5f3e4d703fc75a1c88a4e88e7a
almas <mendygax@gmail.com>
2019-11-08 19:06:57 -0800

    SE FrameWork with Gradle
    for review

---
+++ src/test/java/SeleniumFramework.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumFramework {
+    WebDriver driver = new ChromeDriver();
+
+}
