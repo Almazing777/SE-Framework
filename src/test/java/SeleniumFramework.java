5c037287e7fcec63bffa3d3c6028ea269c209ea9
almas <mendygax@gmail.com>
2019-11-13 11:26:35 -0800

    updated ticket 2
    updated ticket 2 according to the comments

--- src/test/java/SeleniumFramework.java
+++ src/test/java/SeleniumFramework.java
@@ -12,20 +12,25 @@ public class SeleniumFramework {
     public static void LoginSuccess() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://deens.com/?internaltraffic");
-        WebElement login_button = driver.findElement(By.cssSelector("a[href*='login']"));
-        login_button.click();
-        login_button.sendKeys("Mendygax@gmail.com");
+        driver.manage().window().maximize();
         Thread.sleep(1000);
 
-        WebElement password = driver.findElement(By.cssSelector("#password"));
-        password.click();
-        password.sendKeys("Positive89");
+        driver.findElement(By.cssSelector("a[href*='login']")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector("[id=\'email\']")).sendKeys("Mendygax@gmail.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("Positive89");
 
         Thread.sleep(1000);
 
         driver.findElement(By.cssSelector("[data-testid=\'loginSubmit\']")).click();
 
-        Thread.sleep(2000);
+        //Assert.assertTrue(driver.findElement(By.cssSelector("[class=\'ui error message\']")).isDisplayed());
+        Thread.sleep(3000);
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://deens.com/" );
+        System.out.println("Login Successful");
+
         driver.quit();
     }
 
@@ -34,21 +39,20 @@ public class SeleniumFramework {
     public static void WrongEmail() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://deens.com/?internaltraffic");
-
-        WebElement login_button = driver.findElement(By.cssSelector("a[href*='login']"));
-        login_button.click();
-        login_button.sendKeys("mendy@gmail.com");
+        driver.manage().window().maximize();
         Thread.sleep(1000);
 
-        WebElement password = driver.findElement(By.cssSelector("#password"));
-        password.click();
-        password.sendKeys("Positive89");
+        driver.findElement(By.cssSelector("a[href*='login']")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector("[id=\'email\']")).sendKeys("Men@gmail.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("Positive89");
 
         Thread.sleep(1000);
 
         driver.findElement(By.cssSelector("[data-testid=\'loginSubmit\']")).click();
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         Assert.assertTrue(driver.findElement(By.cssSelector("[class=\'ui error message\']")).isDisplayed());
         driver.quit();
@@ -59,12 +63,14 @@ public class SeleniumFramework {
     public static void EmptyCredentials() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://deens.com/?internaltraffic");
+        driver.manage().window().maximize();
 
         driver.findElement(By.cssSelector("a[href*='login']")).click();
 
         Thread.sleep(2000);
 
         driver.findElement(By.cssSelector("[data-testid=\'loginSubmit\']")).click();
+
         Assert.assertTrue(driver.findElement(By.cssSelector("[class=\'ui error message\']")).isDisplayed());
         driver.quit();
     }
